ee0c44416a0762952e4f8fba063c8efc6ac8e6c6
DPR-droid <G00398318@gmit.ie>
2022-11-10 22:21:15 +0000

    Added update gitignore file V2

---
+++ 05_Topic/Lab_06.02_API_Keys.py
@@ -0,0 +1,31 @@
+# In this lab we are going to use Python access APIs that need a key
+# We are using the APIs as described by:
+# • https://html2pdf.app/
+# • https://developer.github.com/v3/guides/
+# NOTE: You will have to get your own key from github
+
+import requests
+import urllib.parse
+from config import config as cfg
+
+targetUrl = "https://en.wikipedia.org"
+#targetUrl = "https://www.atu.ie/"
+
+
+apiKey = cfg["htmltopdfkey"]
+#api = "yourkey"
+apiurl = 'https://api.html2pdf.app/v1/generate'
+
+params = {'url': targetUrl,'apiKey': apiKey}
+parsedparams = urllib.parse.urlencode(params)
+
+requestUrl = apiurl +"?" + parsedparams 
+print (requestUrl)
+
+response = requests.get(requestUrl)
+
+print (response.status_code)
+result =response.content
+
+with open("document.pdf", "wb") as handler:
+    handler.write(result)
